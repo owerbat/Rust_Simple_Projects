d6b011655cc5d15c7ce668ec4169b8e946b7628f
owerbat <niktimak@gmail.com>
2021-08-03 09:55:43 +0300

    Functions

---
+++ functions/src/main.rs
@@ -0,0 +1,29 @@
+fn another_function(x: i32, y: i32) {
+    println!("The value of x is: {}", x);
+    println!("The value of y is: {}", y);
+}
+
+fn five() -> i32 {
+    5
+}
+
+fn plus_one(x: i32) -> i32 {
+    x + 1
+}
+
+fn main() {
+    another_function(5, 6);
+
+    let x = 5;
+    let y = {
+        let x = 3;
+        x + 1
+    };
+    println!("The value of x+y is: {}", x+y);
+
+    let x = five();
+    println!("The value of x is: {}", x);
+
+    let x = plus_one(5);
+    println!("The value of x is: {}", x);
+}
